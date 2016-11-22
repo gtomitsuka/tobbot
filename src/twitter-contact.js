95a714568bfadf732e0a12f00c288552fc22cf2d
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2016-11-22 14:27:24 +0100

    Add fetchDirectMessages() to contact file.

--- src/twitter-contact.js
+++ src/twitter-contact.js
@@ -1,4 +1,4 @@
-var twit = require('../twit');
+var twit = require('twit');
 var config = require('../config');
 
 var T = new twit(config.twit);
@@ -13,4 +13,16 @@ T.get('account/verify_credentials', {
     console.log('successfully connected.')
 }).catch(function (err) {
     console.error('caught error: ', err.stack);
+    throw err;
 });
+
+function fetchDirectMessages(since) {
+    return T.get('direct_messages', {
+        since_id: since,
+        max_id: 200
+    })
+}
+
+module.exports = {
+    fetchDirectMessages
+}
