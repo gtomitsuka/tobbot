626be696340d35fce627be4755b2df3fe003233f
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2016-11-22 15:02:27 +0100

    Add startup routine

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // index.js
 
-// frameworks
-var twitter = require('./src/twitter-contact')
+//internal frameworks
+var twitter = require('./src/twitter-contact');
 var database = require('./src/database/core');
+var messageManager = require('./src/manage-messages')
 
-console.log('hello world!');
+console.log('starting up...');
+
+twitter.connect().then(() => {
+    console.log('successfully connected to Twitter');
+}).then(() => {
+    //returns date
+    return database.queryLastSavedMessage()
+}).then((date) => {
+    console.log('successfully connected to database, last saved message dates to ' + date)
+
+    messageManager.processMessages();
+
+    console.info('successful startup.');
+}).catch(function (err) {
+    console.error('caught error: ', err.stack);
+    throw err;
+});
