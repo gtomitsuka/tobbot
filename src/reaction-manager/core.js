d3921017fa89c019ae8edf1b67b07e6818a3d07c
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2016-11-26 14:34:31 +0100

    Add documentation

--- src/reaction-manager/core.js
+++ src/reaction-manager/core.js
@@ -1,5 +1,13 @@
-// reaction controller - for the moment only "Hi!"
+// reaction controller - at the moment only non-intelligent answers.
 
-module.exports = function () {
+/**
+ * @returns {String} - with simple answers.
+ */
+function basicReply(message) {
+    if (message.text == "How are you doing?")
+        return "Fine, what about you?"
+
     return "Hi!";
 }
+
+module.exports = basicReply
