44533d0d2b60df529b434a97cc742cf32fbbe32a
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2016-11-26 14:34:08 +0100

    Fix id/time issues, add documentation
    There was a problem because JS can only work with numbers until 64-bit, after that it starts to round up numbers and that's a big problem.

--- src/database/core.js
+++ src/database/core.js
@@ -1,22 +1,31 @@
+/*
+ database's core - manages connection to google cloud datastore,
+ keeps methods for writing or querying datastore.
+ */
+
 /* connect to Google Cloud Datastore */
 
 var config = require('../../config');
 
 //external apis
 
-// internal tip
-// use https://googlecloudplatform.github.io/google-cloud-node/#/docs/datastore/0.5.1/datastore
-// as documentation
+// recommended docs: https://googlecloudplatform.github.io/google-cloud-node/#/docs/datastore/0.5.1/datastore
 var Datastore = require('@google-cloud/datastore');
 
 // Instantiate a datastore client
 var datastore = Datastore(config.google);
 
-/*
- database.addDirectMessage(id, senderId, senderName, content, created)
- database.addResponse(id, content, created)
-*/
-
+/**
+ * add direct message to database
+ * @param {String} id - twitter direct message id, send as
+ * string because JS isn't precise with large numbers
+ * @param {Number} senderId - saves senderId to DB for avoiding ambigous
+ * sender name. precision isn't important
+ * @param {String} senderName - for readability and contact purposes.
+ * @param {String} content - DM's text
+ * @param {Date} created - when the message was created
+ * @returns {Promise}
+ */
 function addDirectMessage(id, senderId, senderName, content, created) {
     var messageKey = datastore.key('Message');
 
@@ -32,26 +41,30 @@ function addDirectMessage(id, senderId, senderName, content, created) {
     });
 }
 
+/**
+ * add Tobbot's response to a DM to database
+ * @param {String} id - Twitter's direct message ID
+ * @param {String} content - response's text
+ * @param {Date} created - when was it responded
+ * @returns {Promise}
+ */
 function addResponse(id, content, created) {
     var responseKey = datastore.key('Response');
 
     return datastore.save({
         key: responseKey,
         data: {
-            id,
+            twitter_id: id,
             content,
-            created
+            created: new Date(created)
         }
     });
 }
 
-/*
-    database.fetchLastSavedMessage()
-    last message ist required for not querying messages twice.
-    also used to verify whether system is functioning.
-
-    returns date, when last message was created
- */
+/**
+ * used before fetching DMs and for testing connection
+ * @returns {Object} - with Twitter ID and created at properties. 
+ * */
 function queryLastSavedMessage() {
     var query = datastore.createQuery('Message');
     query.order('created', {
@@ -62,7 +75,11 @@ function queryLastSavedMessage() {
     return datastore.runQuery(query).then((results) => {
         if (results[0].length === 0)
             return
-        return results[0][0].twitter_id;
+
+        return {
+            id: results[0][0].twitter_id,
+            created: results[0][0].created
+        };
     });
 }
 
